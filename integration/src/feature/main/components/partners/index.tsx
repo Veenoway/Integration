649a0714602845a06c2b07b444b336597ab858ef
Veeno <sebastien.cloiseau@gmail.com>
2024-07-19 00:29:15 +0200

    style: responsive partners section

--- integration/src/feature/main/components/partners/index.tsx
+++ integration/src/feature/main/components/partners/index.tsx
@@ -13,13 +13,15 @@ export const Partners = () => {
       <p className="text-[24px] sm:text-[28px] md:text-[32px] lg:text-[38px] text-font-black font-medium max-w-[800px] leading-10 mb-[60px]">
         Nos Partenaires
       </p>
-      <div className="flex items-center flex-wrap justify-center max-w-[1200px] w-[90%]">
+      <div className="flex items-center flex-wrap justify-center max-w-[1300px] w-[90%]">
         {images.map((src, index) => (
           <img
             key={index}
             src={src}
             alt="partner logo"
-            className="object-contain max-w-[140px] w-full grayscale mb-5 mx-2.5"
+            className={`object-contain max-w-[140px] lg:max-w-[190px] w-full grayscale  mx-2.5 lg:mx-[2%] ${
+              index === 1 ? "-mb-[50px]" : " mt-5 lg:mt-9"
+            } `}
           />
         ))}
       </div>
